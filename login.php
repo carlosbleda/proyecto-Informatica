bd8c9e346c63650669f30496016873a45b63da3b
carlosbleda <carlosbleda3@gmail.com>
2022-07-28 19:44:13 -0300

    Initial commit

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./diseño.css">
+</head>
+<body>
+<form action="">
+    <h1>Inicio de sesion</h1>
+    <input type="text" placeholder='Usuario'>
+    <br>
+    <input type="password" placeholder='Constraseña'>
+    <br>
+    <button type="submit">Ingresar</button>
+    <br>
+    <button type="submit">Registrarse</button>
+    
+</body>
+</form>
+</html>
